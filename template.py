130aaca22a9f9a3967c8e3a1a4bfe3d14157fbb0
Vishal <vishal3555gupta@gmail.com>
2026-09-07 10:12:19 +0530

    folder structure is created

---
+++ template.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+import logging
+
+# Logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s]: %(message)s:'
+)
+
+project_name = 'cnnClassifier'
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    # Create parent directories
+    filedir = filepath.parent
+
+    if filedir != Path("."):
+        filedir.mkdir(parents=True, exist_ok=True)
+        logging.info(
+            f"Creating directory: {filedir} for the file: {filepath.name}"
+        )
+
+    # Create file if it doesn't exist or is empty
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        filepath.touch()
+        logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filepath.name} already exists")
